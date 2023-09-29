c1af5c105cfba2977419b659b3bfd23607dc35ce
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-09-29 17:57:54 +0100

    done

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -177,7 +177,7 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Settings for Media
