6f4e3ac92c8af3fadddf8760612c9565514729a5
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-09-29 18:01:55 +0100

    Inner Peace

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -177,8 +177,7 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Settings for Media
 MEDIA_URL = '/media/'
