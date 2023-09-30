121cb44d91a1e273e61f4959adfbab70ccf9fb71
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-09-30 18:27:05 +0100

    Added Static files dirs

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -180,8 +180,9 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / "static",
+]
 # Settings for Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
