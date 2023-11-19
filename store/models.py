d6f3ea9546536ac30ffd2a9687cfbd6c3cb82c0d
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-11-19 20:08:10 +0000

    Updated my sryff

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Address(models.Model):
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
-    whatsapp = models.CharField(max_length=150, verbose_name="Nearest Location")
+    whatsapp = models.CharField(max_length=150, verbose_name="WhatsApp Number")
     lga = models.CharField(max_length=150, verbose_name="City")
     state = models.CharField(max_length=150, verbose_name="State")
 
