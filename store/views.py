3e34a2dfe241a68e5cb264d5d444ca8276035c90
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-11-22 11:30:45 +0000

    updated auth token

--- store/views.py
+++ store/views.py
@@ -231,7 +231,7 @@ def checkout(request):
         # Update order summary for the SMS
         order_summary += f"{bb[0]['whatsapp']}, {bb[0]['state']}, {c.product.title} - Quantity: {c.quantity}, Price: {c.product.price}, {c.product.product_image}\n"
         account_sid = 'AC8377f54cdd5a489e9adee3de643a5317'
-        auth_token = 'c7964a459325604a81e0f5e6bdc6eef0'
+        auth_token = '169201a3bdc3e989f3bacb0b283c655b'
         client = Client(account_sid, auth_token)
         message = client.messages \
             .create(
