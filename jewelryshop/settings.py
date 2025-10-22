006edb8169c78c0c640db6b5eed7089551cc9a86
Ifiok Ambrose <ifiokambrose@gmail.com>
2025-10-22 11:58:29 +0100

    configured repo for railway deployment.

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -23,7 +23,7 @@ cloudinary.config(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
